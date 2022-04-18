361aedb9cf403295216f057404d06900fc1669d7
Chris Castle <chris@render.com>
2022-04-18 14:32:10 -0700

    ✨ Make deployable to Render!

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,14 +1,10 @@
-// learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunction } from "@remix-run/node";
 
 import { prisma } from "~/db.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
-
   try {
-    const url = new URL("/", `http://${host}`);
+    const url = new URL(`http://localhost:${process.env.PORT ?? 3000}/`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
